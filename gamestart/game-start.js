7c563b52228539bc3575f21e7f22cf24dcf3b8de
VivaceJamiel <jimpoy2@gmail.com>
2022-03-03 18:07:34 -0600

    intro

--- gamestart/game-start.js
+++ gamestart/game-start.js
@@ -1,10 +1,36 @@
 function getData() {
     var data = window.localStorage.getItem("name");
     if (data) {
-        var h1 = document.createElement("h1");
-        h1.innerText = data;
-        document.getElementById("container").appendChild(h1);
+        return data;
     }
 }
 
-window.onload = getData;
+function intro() {
+    setTimeout(function () {
+        var name = getData();
+        var first = document.getElementById("first");
+        first.remove();
+        var second = document.createElement("h1");
+        second.setAttribute("id", "second");
+        second.className = "typewriter anim-name";
+        second.innerHTML = name + "...";
+        var container = document.getElementById("response");
+        container.appendChild(second);
+    }, 3000);
+    setTimeout(function () {
+        var second = document.getElementById("second");
+        second.remove();
+        var third = document.createElement("h1");
+        third.setAttribute("id", "third");
+        third.className = "typewriter anim-welcome";
+        third.innerHTML = "Welcome to the game!";
+        var container = document.getElementById("response");
+        container.appendChild(third);
+    }, 6000);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+window.onload = intro;
